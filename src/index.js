b353f38ec07b3593e6928ab982918a5ba4b09cb4
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2018-04-10 08:10:40 -0400

    remove unused resolvers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { Prisma } = require('prisma-binding')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Subscription = require('./resolvers/Subscription')
-const Feed = require('./resolvers/Feed')
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 
 const uuidv4 = require('uuid/v4');
 
