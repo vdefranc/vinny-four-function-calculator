fcb8850fd83f12decf6f49d988e929b98840a6fe
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2018-04-11 11:18:45 -0400

    remove server code from FE repo again

--- src/index.js
+++ src/index.js
@@ -1,71 +1,50 @@
-const { GraphQLServer } = require('graphql-yoga');
-const { Prisma } = require('prisma-binding');
-const fs = require('fs');
-const express = require('express')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+// 1
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import styled from 'styled-components';
 
-const uuidv4 = require('uuid/v4');
+import App from './App';
 
-const resolvers = {
-  Query: {
-    calculator: (parent, args, ctx, info) => {
-      return ctx.db.query.calculators({ where: { id: args.id } })
-    },
-    operations: (parent, args, ctx, info) => {
-      return ctx.db.query.operations({ where: { calculator: {id: args.calculatorId } } })
-    }
-  },
-  Mutation: {
-    addCalculator: (parent, lmfao, ctx, info) => {
-      return ctx.db.mutation.createCalculator({ data: {
-        display: '0',
-        operations: []
-      }});
-    },
-    addOperation: (parent, {calculatorId, operation}, ctx, info) => {
-      return ctx.db.mutation.createOperation({
-        data: {
-          calculator: { connect: { id: calculatorId } },
-          ...operation
-        },
-      }, info);
-    }
-  },
-  Subscription: {
-    newOperation: {
-      subscribe: (parent, args, ctx, info) => {
-        return ctx.db.subscription.operation(
-          // { where: { calculator: { id : args.calculatorId }}},
-          { },
-          info,
-        )
-      }
-    }
-  }
-}
 
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context: req => ({
-    ...req,
-    db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://us1.prisma.sh/public-pintoninja-957/vin-calc/dev',
-      secret: process.env.PRISMA_SECRET,
-      debug: true
-    }),
-  }),
-})
+const httpLink = new HttpLink({
+  uri: 'https://aqueous-mesa-53497.herokuapp.com/'
+});
 
-server.express.use(express.static("client/build"));
-//
-// server.express.get('/', (req, res) => {
-//   res.render('client/build/index');
-//   // return fs.readFile('client/build/index.html', 'utf-8', (err, data) => {
-//   //    res.writeHead(200, { "Content-Type": "text/html" });
-//   // });
-// })
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
-server.start({ port: process.env.PORT || 4000 },
-  () => console.log(`Server is running on port ${process.env.PORT || 4000}`)
+const Header = styled.h1`
+  position: relative;
+  margin: 0;
+  font-family: "Titillium Web", sans-serif;
+  font-size: 200px;
+  font-style: italic;
+  line-height: 1;
+  font-size: 4em;
+  background-image: linear-gradient(#378dbc 0%, #b6e8f1 46%, #fff 50%, #32120e 54%, #ffc488 58%, #582c11 90%, #ec9b4e 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke: 1px #f975f7;
+  filter: drop-shadow(1px 1px 3px #f008b7);
+  margin-bottom: 0.5em;
+`;
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Header>Vinny's Cool Calculator</Header>
+
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
 );
+
+registerServiceWorker();
